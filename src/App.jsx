050b494a9f90998534bc6645045a11aec0ef900c
Vishal Kumar <vishalfreelancing903@gmail.com>
2024-04-07 12:05:39 +0500

    main

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { counterStyles } from "./styles";
 import "./App.css";
 
 function App() {
+  console.log(localStorage.getItem("count"));
   const [count, setCount] = useState(0);
 
   const increamentCount = () => setCount(count + 1);
