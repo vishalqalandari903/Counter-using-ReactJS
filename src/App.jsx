a40afcfc5bc5a60474509554e33e45dc23304baf
Vishal Kumar <vishalfreelancing903@gmail.com>
2024-04-09 16:40:37 +0500

    theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,6 @@ import "./App.css";
 import { Counter, Header } from "./components";
 
 function App() {
-  // const [themeMode, setThemeMode] = useState("dark");
-  const [themeMode, setThemeMode] = useState("light");
-
-  useEffect(() => {
-    document.querySelector("html").classList.remove("light", "dark");
-    document.querySelector("html").classList.add(themeMode);
-  }, [themeMode]);
   return (
     <>
       <Header />
